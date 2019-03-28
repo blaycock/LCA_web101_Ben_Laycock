09c0532e16f2e67a34247cbee29b4e2aee5ce12f
blaycock <ben.laycock@texastechfcu.org>
2019-03-27 20:25:00 -0500

    changes on tictactoe

--- myPortfolio/scripts/tictactoe.js
+++ myPortfolio/scripts/tictactoe.js
@@ -1,3 +1,24 @@
-var gameMarker = 'x';
-function changeMarkerToX() {}
-function changeMarkerToO() {}
+// A $( document ).ready() block.
+$(document).ready(function() {
+	let player1 = 'X';
+	let player2 = 'O';
+
+	let currentTurn = 1;
+	let movesMade = 0;
+
+	let sqr = $('.square');
+
+	sqr.on('click', function(c) {
+		movesMade++;
+
+		if (currentTurn === 1) {
+			event.target.innerHTML = player1;
+			event.target.style.color = 'red';
+			currentTurn++;
+		} else {
+			event.target.innerHTML = player2;
+			event.target.style.color = 'blue';
+			currentTurn--;
+		}
+	});
+});
