d6012c54e092bced0d871ba919651093172289a2
blaycock <ben.laycock@texastechfcu.org>
2019-04-21 14:13:57 -0500

    Task List beginning

---
+++ myPortfolio/aUdemy/JavaScript/TASKLIST/app.js
@@ -0,0 +1,24 @@
+// Define UI Vars
+const form = document.querySelector("#task-form");
+const tasklist = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener("submit", addTask);
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  e.preventDefault();
+}
